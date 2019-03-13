0555ee38f95ddc4f2a0cffffc3b33961a7c0d7d2
MythaelX <even.clement@live.fr>
2019-03-13 11:27:16 +0100

    adrien est mauvais en maths

--- regulation.c
+++ regulation.c
@@ -2,12 +2,15 @@
 	 
 	float regulationTest(int regul, float csgn, float* tabT, int nT){
 		float cmd=100.0;
-		float* integrale = NULL;
-		integrale = 0;
+		float* integrale = malloc(sizeof(float));
+		*integrale = 0;
+		
 		int i = 0;
 		for(i = 0 ; i < nT ; i++){
-			cmd = regulation(regul, csgn, tabT[i], tabT[i > 0 ? i - 1 : i], &integrale);
-		}
+			cmd = regulation(regul, csgn, tabT[i], tabT[i > 0 ? i - 1 : i], integrale);
+			//printf("integrale : %f\n",*integrale);
+		} 
+		free(integrale);
 		return cmd;
    }
     
@@ -27,18 +30,23 @@ float regulation(int regul, float csgn, float tN, float tNm1, float* integrale){
 		case 2:
 			p = csgn - tN;
 			//printf("p : %f\n", p);
-			if(tN == tNm1){
-				i = 0;
-			}else if(p > 0){
-				i = (dt * ((tN < tNm1 ? tN : tNm1) - csgn)) + ((((tN - tNm1) * ((tN >= tNm1) ? 1 : -1))*dt) / 2);
-			}else if(p < 0){
-				i = (dt * ((tN )))
+			if(tN < tNm1){
+				i = ((csgn-tNm1)*dt)+(tNm1-tN)*dt/2;				
+			}else if(tN > tNm1){
+				i = (dt*(csgn-tN))+(tN-tNm1)*dt/2;
+			}else{
+				i=0;
 			}
-				*integrale += i;
-			printf("i : %f\n", i);
-			d = (tN - tNm1) / dt;
+			*integrale += i;
+			//printf("i : %f\n", i);
+			d = (tNm1 - tN) / dt;
 			//printf("d : %f\n", d);
-			cmd = p + i*0.1 + d*0.1;
+			cmd = p + *integrale*0.1 + d*0.1;
+			if(cmd<0){
+				cmd=0;
+			}else if(cmd>100){
+				cmd=100;
+			}
 			break;
 	}
 	return cmd;
