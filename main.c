f7a2f68410590bcf6f62bb35a7b943fb03bf3a2b
MythaelX <even.clement@live.fr>
2019-03-13 15:53:08 +0100

    adrien est mauvais en c

--- main.c
+++ main.c
@@ -1,33 +1,48 @@
 //#include <stdio.h>
 #include "releve.h"
 #include "commande.h"
+#include "define.h"
+#include "integration.h"
+#include "regulation.h"
+#include "visualisationT.h"
+#include "visualisationC.h"
+#include "consigne.h"
 int main(/*int argc, char *argv[]*/){
-    int i=0;
+   /* float* integrale = malloc(sizeof(float));
+    float csgn, cmd;
     FT_HANDLE ftHandle;
-    FT_STATUS ftStatus;
-    printf("ok\n");
-    temp_t temperatures;
-
+    FT_STATUS ftStatus; */
+    temp_t temperatures, temperatures_Acienne;
+    /*
     //verif port usb connecte
     ftStatus = FT_Open(0, &ftHandle);
     if(ftStatus != FT_OK) {
         printf("\nEchec de la lecture usb");
         return;
     }
-    
+    *integrale = 0;
     temperatures = releve(ftHandle,ftStatus);
-    printf("\next :%lf int :%lf\n",temperatures.exterieure,temperatures.interieure);
-    printf("ok2\n");
-    commande(ftHandle,ftStatus,10.00);
-    while(1){
-        commande(ftHandle,ftStatus,i);
+    commande(ftHandle,ftStatus,0);
+    csgn = consigne(csgn);
+    if(csgn<5){
+      return 0;
+    }*/
+    temperatures.exterieure=0;
+    temperatures.interieure=0;
+    visualisationT(temperatures);
+    visualisationC(0);
+    /*while(1){     
+        visualisationT(temperatures);
+        csgn = consigne(csgn);
+        cmd = regulation(1, csgn, temperatures.interieure, temperatures_Acienne.interieure, integrale,2);
+        visualisationC(cmd);
+        temperatures_Acienne=temperatures;
         temperatures = releve(ftHandle,ftStatus);
         printf("\next :%lf int :%lf\n",temperatures.exterieure,temperatures.interieure);
-        i++;
-        if(i==100){
-            i=0;
-        }
-    } 
+        printf("\nconsignes:%lf\n",temperatures.exterieure,temperatures.interieure);
+    } */
     FT_Close(ftHandle);
+    free(integrale);
+    return 0;
 }
 
--- releve.h
+++ releve.h
@@ -1,10 +1,5 @@
 #include <stdio.h>
 #include "lib/ftd2xx.h"
-typedef struct temp_t temp_t;
-struct temp_t{
-    float interieure;
-    float exterieure;
-};
-
+#include "define.h"
 temp_t releve(FT_HANDLE ftHandle,FT_STATUS ftStatus);
 
