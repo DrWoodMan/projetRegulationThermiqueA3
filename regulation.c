2a79c19b7a15206579e8b1e00d0945e19d7394e8
MythaelX <even.clement@live.fr>
2019-03-13 15:01:41 +0100

    adrien est mauvais en c

--- regulation.c
+++ regulation.c
@@ -7,13 +7,13 @@
 		
 		int i = 0;
 		for(i = 0 ; i < nT ; i++){
-			cmd = regulation(regul, csgn, tabT[i], tabT[i > 0 ? i - 1 : i], integrale);
+			cmd = regulation(regul, csgn, tabT[i], tabT[i > 0 ? i - 1 : i], integrale,1);
 		} 
 		free(integrale);
 		return cmd;
    }
     
-float regulation(int regul, float csgn, float tN, float tNm1, float* integrale){
+float regulation(int regul, float csgn, float tN, float tNm1, float* integrale,int mode_PID){
 	int dt = 10;
 	float p, i, d, cmd;
 
@@ -38,8 +38,11 @@ float regulation(int regul, float csgn, float tN, float tNm1, float* integrale){
 			*integrale += i;
 
 			d = (tNm1 - tN) / dt;
-
-			cmd = p*2 + *integrale*0.5 + d*0.1;
+			if(mode_PID==1){
+				cmd = p + *integrale*0.1 + d*0.1;
+			}else if(mode_PID==2){
+				cmd = p*6 + *integrale*0.5 + d*0.1;
+			}
 			if(cmd<0){
 				cmd=0;
 			}else if(cmd>100){
--- regulation.h
+++ regulation.h
@@ -1,4 +1,4 @@
 	#include "define.h" 
     
 	float regulationTest(int regul,float csgn,float* tabT, int nT);
-	float regulation(int regul, float csgn, float tN, float tNm1, float* integrale);
+	float regulation(int regul, float csgn, float tN, float tNm1, float* integrale,int mode_PID);
